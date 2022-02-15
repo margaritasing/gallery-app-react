89d5dcb15b5388a31cf052e6599457ec33fd138b
margaritasing <libiamfreitesm@gmail.com>
2022-02-15 16:10:04 -0300

    Agrego componentes, card, cards y container

---
+++ src/component/Container.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Container = () => {
+  return (
+    <div className="container border">
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://images.unsplash.com/photo-1643721859448-4d241d04448b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDE5NDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDQ5NDkzODA&ixlib=rb-1.2.1&q=80&w=1080"
+          className="card-img-top"
+          alt="imagen.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Container;
