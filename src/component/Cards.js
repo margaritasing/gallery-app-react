99353e36f1915b8e7fe47b0ffa70efbf06ca7294
margaritasing <libiamfreitesm@gmail.com>
2022-02-15 16:38:06 -0300

    agrego cambios en las card y cads

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,7 +1,30 @@
-import React from "react";
+import { cleanup } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  const [images, setImages] = useState({});
+
+  const peticion = async () => {
+    const res = await fetch(
+      "https://api.unsplash.com/photos/random/?client_id=ZYRLj7ZrlGTA2w6S1SOu_THQRZQvmTA4IlJMk-2rVQ0"
+    );
+
+    const data = await res.json();
+    console.log(data);
+
+    setImages(data);
+  };
+
+  useEffect(() => {
+    peticion();
+  }, []);
+
+  return (
+    <div className="cards">
+      <Card img={images.urls.regular} />
+    </div>
+  );
 };
 
 export default Cards;
