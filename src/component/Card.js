97a1dd7e87f07b92348dd9f1d1ef4fd89254f52c
margaritasing <libiamfreitesm@gmail.com>
2022-02-16 00:13:56 -0300

    agrego conexion con la api

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const Card = ({ img }) => {
   return (
     <div>
-      <div className="card" style={{ width: "18rem" }}>
+      <div className="card m-2" style={{ width: "20rem", height: "auto" }}>
         <img src={img} alt="imagen.png" />
       </div>
     </div>
--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,30 +1,65 @@
 import { cleanup } from "@testing-library/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Card from "./Card";
 
 const Cards = () => {
   const [images, setImages] = useState([]);
+  const [input, setInput] = useState("");
 
-  const peticion = async () => {
-    const res = await fetch(
-      "https://api.unsplash.com/photos/?client_id=ZYRLj7ZrlGTA2w6S1SOu_THQRZQvmTA4IlJMk-2rVQ0"
-    );
+  const peticion = useCallback(async () => {
+    const key = "client_id=ZYRLj7ZrlGTA2w6S1SOu_THQRZQvmTA4IlJMk-2rVQ0";
+
+    let route = `https://api.unsplash.com/photos/?${key}`;
+
+    if (input !== "") {
+      route = `https://api.unsplash.com/search/photos/?query=${encodeURI(
+        input
+      )}&${key}`;
+    }
+
+    let res = await fetch(route);
 
     const data = await res.json();
-    console.log(data);
 
-    setImages(data);
-  };
+    if (data.results) {
+      setImages(data.results);
+    } else {
+      setImages(data);
+    }
+  }, [input]);
 
   useEffect(() => {
     peticion();
-  }, []);
+  }, [input, peticion]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const text = e.target[0].value;
+
+    setInput(text);
+  };
 
   return (
     <div className="cards">
-      {images.map((img) => {
-        return <Card key={img.id} img={img.urls.regular} />;
-      })}
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <label className="w-75">
+          Buscar: <input className="w-100" type="text" name="inputText" />
+        </label>
+        <button type="submit" className="btn btn-warning m-2">
+          <span className="material-icons">search</span>
+        </button>
+      </form>
+      <hr />
+      <div className="row">
+        {images.map((img) => {
+          return (
+            <div className="col">
+              <Card key={img.id} img={img.urls.regular} />;
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
